2acf74ac6056662e7634e870b8130a319e85bfb8
Aditya1634 <adityadhage66927@gmail.com>
2025-04-23 17:01:44 +0530

    Added Star patterns

---
+++ Loops_Patterns/Pattern1.java
@@ -0,0 +1,19 @@
+package Loops_Patterns;
+
+//1 2 3 4 5
+//1 2 3 4 5
+//1 2 3 4 5
+//1 2 3 4 5
+//1 2 3 4 5
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Methods/StaticMethodDemo/Circle.java
@@ -0,0 +1,5 @@
+package Methods.StaticMethodDemo;
+
+public class Circle {
+	
+}
