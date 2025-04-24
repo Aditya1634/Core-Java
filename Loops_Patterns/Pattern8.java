d30159d6df61461564e5dde34b769ee852609fd3
Aditya1634 <adityadhage66927@gmail.com>
2025-04-24 17:24:15 +0530

    Added Star Patterns

--- Loops_Patterns/Pattern8.java
+++ Loops_Patterns/Pattern8.java
@@ -1,18 +1,17 @@
 package Loops_Patterns;
-//     *
-//    *
-//   *
-//  *
-// *
-public class Pattern4 {
+
+public class Pattern8 {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int n = 5;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				if(i+j==n+1)System.out.print("*");
-				else System.out.print(" ");
+				if(i+j==n+1 || j==n || i==n)System.out.print("* ");
+				else System.out.print("  ");
 			}
 			System.out.println();
 		}
 	}
+
 }
---
+++ Loops_Patterns/Pattern9.java
@@ -0,0 +1,19 @@
+package Loops_Patterns;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = n-i; j > 0; j--) {
+				
+				if()
+				System.out.print("* ");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
